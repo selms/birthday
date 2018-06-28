b7a890ed11f717481cd817392307004eeacb167a
selms <selmsb@gmail.com>
2018-06-28 09:28:17 -0600

    added all the files

---
+++ src/components/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+    <div className="grid">
+      <div className="grid__skew-dark-one-box"></div>
+      <div className="grid__skew-dark-two"></div>
+      <div className="grid__skew-dark-three"></div>
+
+      <div className="grid__skew-light-one"></div>
+      <div className="grid__skew-light-two"></div>
+      <div className="grid__skew-light-three-box"></div>
+        
+      </div>
+    );
+  }
+}
