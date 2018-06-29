6c5a380303aee8d511e181b27f19c18be84cf545
selms <selmsb@gmail.com>
2018-06-29 14:36:20 -0600

    style fixes

--- src/components/app.js
+++ src/components/app.js
@@ -38,8 +38,8 @@ export default class App extends Component {
     <div className="grid">
      <h1 className="grid__title">Birthday Countdown</h1>
 
-      <div className="grid__skew-dark-one-box"></div>
-      <div className="grid__skew-dark-two"></div>
+    
+      <div className="grid__skew-dark-two"></div>}
       <div className="grid__skew-dark-three"></div>
 
       <div className="grid__skew-light-one"></div>
